d3f50b7f749c27c22ef8c2db9420f47b0db1b9b3
vnfmsqkek3 <czy1023@mz.co.kr>
2025-04-17 17:50:16 +0900

    chore: 버전 0.1.1로 업데이트 및 uuid 의존성 제거

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nr-bedrock-observability",
-    version="0.1.0",
+    version="0.1.1",
     description="New Relic observability for AWS Bedrock",
     author="New Relic",
     author_email="opensource@newrelic.com",
