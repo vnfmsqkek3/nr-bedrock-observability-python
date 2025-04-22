c83a864ee8cbac4d4af3569f0cf37c3411149708
vnfmsqkek3 <czy1023@mz.co.kr>
2025-04-22 15:11:45 +0900

    재귀적 순환 참조 문제 해결

--- src/__init__.py
+++ src/__init__.py
@@ -7,12 +7,16 @@ from .monitor import monitor_bedrock
 __version__ = "0.1.3"
 __all__ = ["monitor_bedrock"]
 
-# 직접 import할 수 있도록 최상위 레벨에 노출
-def monitor_bedrock_wrapper(*args, **kwargs):
-    """
-    monitor_bedrock 함수의 래퍼
-    """
-    return monitor_bedrock(*args, **kwargs)
+# 직접 import를 위한 최상위 레벨 참조는 필요하지 않음
+# monitor_bedrock이 이미 .monitor에서 import되어 있으므로
+# 아래의 코드 제거
 
-# 호환성을 위해 기존 이름도 유지
-monitor_bedrock = monitor_bedrock_wrapper 
+# # 직접 import할 수 있도록 최상위 레벨에 노출
+# def monitor_bedrock_wrapper(*args, **kwargs):
+#     """
+#     monitor_bedrock 함수의 래퍼
+#     """
+#     return monitor_bedrock(*args, **kwargs)
+
+# # 호환성을 위해 기존 이름도 유지
+# monitor_bedrock = monitor_bedrock_wrapper 
